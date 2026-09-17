2dd130a244fb15a843094bab7ea156e36ffbb923
chez-shanpu <tomoki.sugiura@mail.shanpu.info>
2026-09-17 21:47:43 +0900

    Add Apache-2.0 SPDX and copyright headers to Go files
    Signed-off-by: chez-shanpu <tomoki.sugiura@mail.shanpu.info>

--- doc.go
+++ doc.go
@@ -1,3 +1,6 @@
+// SPDX-License-Identifier: Apache-2.0
+// Copyright Authors of typesafeai-go
+
 // Package typesafeai provides a Go client for the TypeSafe AI API.
 // https://docs.typesafe.ai/api
 package typesafeai
